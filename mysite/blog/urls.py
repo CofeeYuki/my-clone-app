b125edc8b9d18785254f7a8598d5ffcf645326a6
CofeeYuki <studycoffee2001@gmail.com>
2020-10-03 16:55:43 +0900

    My first commit

---
+++ mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+]
